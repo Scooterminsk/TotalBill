ecb43794822e4bf6820375173b33d780e52a86c6
Scooterminsk <kickscooterby@gmail.com>
2022-09-29 19:41:50 +0300

    Project preparing

---
+++ TotalBill/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TotalBill
+//
+//  Created by Zenya Kirilov on 29.09.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        
+    }
+
+
+}
+
+extension MainViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+        
+        
+        ])
+    }
+    
+}
