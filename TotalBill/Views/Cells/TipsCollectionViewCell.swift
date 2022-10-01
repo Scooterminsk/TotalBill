01a34fbcb0465b1050073c47a4a4e852973d7a69
Scooterminsk <kickscooterby@gmail.com>
2022-10-01 17:18:37 +0300

    Created folders for better navigation

---
+++ TotalBill/Views/Cells/TipsCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  TipsCollectionViewCell.swift
+//  TotalBill
+//
+//  Created by Zenya Kirilov on 1.10.22.
+//
+
+import Foundation
---
+++ TotalBill/Views/TipsCollectionView.swift
@@ -0,0 +1,11 @@
+//
+//  TipsCollectionView.swift
+//  TotalBill
+//
+//  Created by Zenya Kirilov on 1.10.22.
+//
+
+import Foundation
+import UIKit
+
+
