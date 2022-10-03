62d15c4ad749ce52446ab613b9d15798b256d479
Scooterminsk <kickscooterby@gmail.com>
2022-10-03 23:23:27 +0300

    Total bill view fonts settings

--- TotalBill/Views/TotalBillView.swift
+++ TotalBill/Views/TotalBillView.swift
@@ -14,7 +14,6 @@ class TotalBillView: UIView {
         let label = UILabel()
         label.text = "Total bill"
         label.textColor = #colorLiteral(red: 0.2454499006, green: 0.2894837558, blue: 0.3496103287, alpha: 1)
-        label.font = UIFont(name: "Avenir Next", size: 14)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -24,7 +23,6 @@ class TotalBillView: UIView {
         textField.backgroundColor = #colorLiteral(red: 0.9561659694, green: 0.9591339231, blue: 0.9530903697, alpha: 1)
         textField.layer.cornerRadius = 10
         textField.textColor = .black
-        textField.font = UIFont(name: "Avenir Next Bold", size: 48)
         textField.textAlignment = .center
         textField.keyboardType = .numberPad
         textField.tintColor = #colorLiteral(red: 0.4549019608, green: 0.08235294118, blue: 0.7058823529, alpha: 1)
@@ -42,6 +40,13 @@ class TotalBillView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        titleLabel.font = UIFont(name: "Avenir Next", size: frame.width / 26.7)
+        summTextField.font = UIFont(name: "Avenir Next Bold", size: frame.height / 2.6)
     }
     
     private func setupView() {
